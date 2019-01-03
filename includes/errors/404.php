f0720619010211d79a85624e868a66a0ad43b2b0
amrmuhammed <46347814+amrmuhammed@users.noreply.github.com>
2019-01-03 15:48:26 +0200

    Add files via upload

---
+++ includes/errors/404.php
@@ -0,0 +1,14 @@
+
+    <div class="error-404">
+      <div class="container">
+        <div class="h1 bold">
+          ERROR 404:
+        </div>
+        <p class="lead h3">
+          ERROR 404: PAGE NOT FOUND
+        </p>
+      </div>
+    </div>
+
+
+<?php require_once('footer.php'); ?>
---
+++ includes/welcome.php
@@ -0,0 +1,3 @@
+<div class="welcome-page">
+  WELCOME NEW USER
+</div>
